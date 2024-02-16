3b5c321bc2158cc493131090a440cc7cdb6739ab
leemjaejun <156068889+JJleem@users.noreply.github.com>
2024-02-16 09:49:34 +0900

    Add files via upload

---
+++ 240216/2/dist/script.dev.js
@@ -0,0 +1,51 @@
+"use strict";
+
+$("#pic1").hover(function () {
+  // mouseover
+  $(this).attr("src", "../img/2.hobby/2.reaptile/백룡1.jpg");
+}, function () {
+  // mouseout
+  $(this).attr("src", "../img/2.hobby/2.reaptile/백룡2.jpg");
+});
+$("#pic").hover(function () {
+  // mouseover
+  $(this).attr("src", "../img/2.hobby/2.reaptile/참깨1.jpg");
+}, function () {
+  // mouseout
+  $(this).attr("src", "../img/2.hobby/2.reaptile/참깨2.jpg");
+});
+$("#pic2").hover(function () {
+  // mouseover
+  $(this).attr("src", "../img/2.hobby/2.reaptile/튼튼1.jpg");
+}, function () {
+  // mouseout
+  $(this).attr("src", "../img/2.hobby/2.reaptile/튼튼2.jpg");
+});
+$("#pic3").hover(function () {
+  // mouseover
+  $(this).attr("src", "../img/2.hobby/2.reaptile/뿌치1.jpg");
+}, function () {
+  // mouseout
+  $(this).attr("src", "../img/2.hobby/2.reaptile/뿌치2.jpg");
+});
+$("#pic4").hover(function () {
+  // mouseover
+  $(this).attr("src", "../img/2.hobby/2.reaptile/레오1.jpg");
+}, function () {
+  // mouseout
+  $(this).attr("src", "../img/2.hobby/2.reaptile/레오2.jpg");
+});
+$("#pic5").hover(function () {
+  // mouseover
+  $(this).attr("src", "../img/2.hobby/2.reaptile/티티1.jpg");
+}, function () {
+  // mouseout
+  $(this).attr("src", "../img/2.hobby/2.reaptile/티티2.jpg");
+});
+$("#pic6").hover(function () {
+  // mouseover
+  $(this).attr("src", "../img/2.hobby/2.reaptile/나나1.jpg");
+}, function () {
+  // mouseout
+  $(this).attr("src", "../img/2.hobby/2.reaptile/나나2.jpg");
+});
